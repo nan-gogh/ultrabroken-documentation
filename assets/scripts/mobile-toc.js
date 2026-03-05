aa2abd316ffccf697aa950e3d782c334a5717432
nan-gogh <nan-gogh@users.noreply.github.com>
2026-03-05 22:33:59 +0000

    deploy: 1c3289ab8810887d5244ed35a546742d9a1a44cb

--- assets/scripts/mobile-toc.js
+++ assets/scripts/mobile-toc.js
@@ -25,7 +25,7 @@
     var btn = document.createElement('button');
     btn.className = 'ub-toc-fab';
     btn.setAttribute('aria-label', 'Table of contents');
-    btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M3 9h14V7H3v2m0 4h14v-2H3v2m0 4h14v-2H3v2m16 0h2v-2h-2v2m0-10v2h2V7h-2m0 6h2v-2h-2v2z"/></svg>';
+    btn.innerHTML = '<span class="md-nav__icon md-icon"></span>';
     document.body.appendChild(btn);
 
     // Create backdrop
